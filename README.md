34b5a17bbf6955651921afd1e0468430adc46a65
Jo Van Eyck <j.vaneyck@farmad.be>
2022-11-06 14:02:38 +0100

    added F# starting point

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Starting code for the "introduction to property based testing" workshop
 
 ## kata description
 You can find the original kata description [here](https://kata-log.rocks/mars-rover-kata).
+
+## Workspaces
+
+This repository contains projects you can run in your favorite IDE. If you don't have an IDE at the ready, you can also try this exercise in an online repl.it. Click on your preferred language and **"Fork Repl"** 
+
+* [C# repl.it](https://replit.com/@praGmatic/pbt-csharp?v=1)
+* [F# repl.it](https://replit.com/@praGmatic/pbt-fsharp?v=1)
---
+++ fsharp/rover.fsx
@@ -0,0 +1,77 @@
+#r "nuget: fscheck"
+#r "nuget: Unquote"
+
+open System
+open FsCheck
+open Swensen.Unquote
+
+printfn "Hello World from F#!"
+
+type Direction =
+    | N
+    | E
+    | S
+    | W
+
+type Location = int * int
+
+type Rover =
+    { Location: Location
+      Direction: Direction }
+
+type Command =
+    | F
+    | B
+    | L
+    | R
+
+let moveForward r =
+    let x, y = r.Location
+
+    match r.Direction with
+    | N -> { r with Location = (x + 1, y) }
+    | S -> { r with Location = (x - 1, y) }
+    | E -> { r with Location = (x, y + 1) }
+    | W -> { r with Location = (x, y - 1) }
+
+let moveBackward r =
+    let x, y = r.Location
+
+    match r.Direction with
+    | N -> { r with Location = (x - 1, y) }
+    | S -> { r with Location = (x + 1, y) }
+    | E -> { r with Location = (x, y - 1) }
+    | W -> { r with Location = (x, y + 1) }
+
+let rotateLeft (r: Rover) =
+    match r.Direction with
+    | N -> { r with Direction = W }
+    | E -> { r with Direction = N }
+    | S -> { r with Direction = E }
+    | W -> { r with Direction = S }
+
+let rotateRight (r: Rover) =
+    match r.Direction with
+    | N -> { r with Direction = E }
+    | E -> { r with Direction = S }
+    | S -> { r with Direction = W }
+    | W -> { r with Direction = N }
+
+let execute (r: Rover) (c: Command) =
+    match c with
+    | F -> moveForward r
+    | B -> moveBackward r
+    | L -> rotateLeft r
+    | R -> rotateRight r
+
+let run (r: Rover) (cs: Command list) = cs |> List.fold execute r
+
+(*Example of a failing property:
+
+A command always changes a rover-Falsifiable, after 1 test (0 shrinks) (StdGen (1772415496, 297107466)):
+Original:
+{ Location = (0, 0)
+  Direction = W }
+F
+*)
+Check.Quick("A command always changes a rover", (fun r c -> execute r c = r)) //TODO FIXME
