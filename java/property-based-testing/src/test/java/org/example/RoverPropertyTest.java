bf37db4e33b1828a18dc51ca4cfdf92c96641ba0
Axel Verhaeghe <verhaeghe.a.j@gmail.com>
2022-12-01 12:05:03 +0100

    Add property-based-test example

---
+++ java/property-based-testing/src/test/java/org/example/RoverPropertyTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.Rover.roverBuilder;
+
+public class RoverPropertyTest {
+
+    @Property
+    void anyCommand_roverAlwaysChanged(@ForAll Command command) {
+        final Rover rover = roverBuilder().build();
+
+        rover.processCommand(command);
+
+        assertThat(rover).isNotEqualTo(roverBuilder().build());
+    }
+}
